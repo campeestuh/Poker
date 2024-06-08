311abb1731059704a10cea28bde27740b999726f
Angel <124530206+an9le@users.noreply.github.com>
2024-06-07 19:47:13 -0700

    Update Deck.java

--- Poker/src/DeckofCards/Deck.java
+++ Poker/src/DeckofCards/Deck.java
@@ -3,34 +3,31 @@ package DeckofCards;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
-
 public class Deck {
 	
 	private ArrayList<Cards> cards;
 	
-	
 	public Deck() {
-	setCards(new ArrayList<Cards>());
+		setCards(new ArrayList<Cards>());
 	}
 	
-	  
-	 public void initDeck() {
-	        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
-	        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+	public void initDeck() {
+	    String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+	    String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 
-	        for (String suit : suits) {
-	            for (String rank : ranks) {
-	                Cards card = new Cards(rank, suit);
-	                cards.add(card);
-	                System.out.println(card.Name()); // Print the name of each card in the shuffled deck
-	            }
+	    for (String suit : suits) {
+	        for (String rank : ranks) {
+	            Cards card = new Cards(rank, suit);
+	            cards.add(card);
+                System.out.println(card.Name()); // Print the name of each card in the shuffled deck
 	        }
-	 }
+	    }
+	}
 	
 	public ArrayList<Cards> getCards() {
 		return cards;
 	}
+	
 	public void setCards(ArrayList<Cards> cards) {
 		this.cards = cards;
 	}
@@ -40,7 +37,6 @@ public class Deck {
         for (Cards card : cards) {
             System.out.println(card.Name()); // Print the name of each card in the shuffled deck
         }
-		
 	}
 
 	public int size() {
